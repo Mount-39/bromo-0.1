8560411e2419416cdb0be7d03bb4af8d62ae5ea2
Ulyanov <dominic_u@ukr.net>
2015-11-14 17:17:21 +0200

    [UPDATE]

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var port = 8080,
-    express = require('express');
-
-express().use(express.static(__dirname + '/public'))
-         .listen(port);
-
-console.log('Server is running on port: ' + port);
+// modules
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
