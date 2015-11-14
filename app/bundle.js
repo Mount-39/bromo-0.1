6d42806e5f86d92e6c2ec90af804b033eeb55442
Ulyanov <dominic_u@ukr.net>
2015-11-14 21:25:59 +0200

    [UPDATE]

--- app/bundle.js
+++ app/bundle.js
@@ -9211,4 +9211,13 @@ return jQuery;
 
 (function () {
     var socket = io();
+
+    $(document).ready(function () {
+        $('form').submit(function(){
+            var m = $('#m');
+            socket.emit('chat message', m.val());
+            m.val('');
+            return false;
+        });
+    });
 })();
--- core/index.js
+++ core/index.js
@@ -1,3 +1,12 @@
 (function () {
     var socket = io();
+
+    $(document).ready(function () {
+        $('form').submit(function(){
+            var m = $('#m');
+            socket.emit('chat message', m.val());
+            m.val('');
+            return false;
+        });
+    });
 })();
