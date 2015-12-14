a742f63bbf422393b7be68f2bd941b6d99dd7c06
Dmitry Ulyanov <dominic_u@ukr.net>
2015-12-14 17:04:51 +0200

    [UPDATE]

--- run.js
+++ run.js
@@ -1,71 +1,66 @@
-// MODULES
-// setting up server etc.
-var express = require('express'),
-    session = require('express-session'),
-    bodyParser = require('body-parser');
-    cookieParser = require('cookie-parser'),
-    app = express(),
-    router = express.Router(),
-    http = require('http').Server(app);
+// SETTING UP THE SERVER
+var express = require('express');
+var router = express.Router();
+var app = express();
+var http = require('http').Server(app);
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var parser = require('body-parser');
+///////////////////////////////////////////
 
-// mongoDB
-var mongoose = require('mongoose'),
-    mongoStore = require('connect-mongo')(session);
-
-// socket
+// SETTING UP SOCKET.IO
 var io = require('socket.io')(http);
+///////////////////////////////////////////
 
-// VARIABLES
-// ==================================
+// SETTING UP MONGO
+var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(session);
+mongoose.connect('mongodb://mount39:mount39@ds049651.mongolab.com:49651/bromo');
+///////////////////////////////////////////
+
+// SETTING UP PORT AND STATIC FOLDER
 var port = process.env.PORT || 8080,
     folder = __dirname + '/app/';
+///////////////////////////////////////////
 
-// CONNECT TO MONGO
-mongoose.connect('mongodb://mount39:mount39@ds049651.mongolab.com:49651/bromo');
-
-// SETTING SESSION
+// SETTING UP COOKIES & SESSION
 app.use(cookieParser());
 app.use(session({
-    secret: 'mount39',
+    secret: 'r2d2',
     store: new mongoStore({
         mongooseConnection: mongoose.connection
     })
 }));
+///////////////////////////////////////////
 
-// SETTING UP ROUTES
-// ==================================
-// setting up router event on request
+// CONFIGURING ROUTES
 router.use(function (req, res, next) {
-    //console.log(req.method, req.url);
+    console.log(req.method, req.url);
     next();
 });
 router.get('/', function (req, res) {
     res.sendFile(folder + 'index.html');
 });
-router.get('/favicon.icon', function (req, res) {
-    res.sendFile(folder + './style/img/favicon.ico');
-});
 router.use('/js', express.static(folder + '/js'));
 router.use('/style', express.static(folder + '/style'));
-
-// applying routes to app
 app.use('/', router);
+///////////////////////////////////////////
 
-// configuring socket.io
+// CONFIGURING SOCKET.IO
 io.on('connect', function (socket) {
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
-    // sending message
     socket.on('message', function (message) {
         socket.broadcast.emit('message', message);
     });
 
 });
+///////////////////////////////////////////
 
-// listening port
+// START UP SERVER
 http.listen(port, function () {
     console.log('Bromo is running on port ' + port);
 });
