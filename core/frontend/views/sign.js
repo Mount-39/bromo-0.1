940b72c113314f23513dca247dbb2d16050baefe
NeedSomeMeat <htc.x.boy@gmail.com>
2015-12-22 22:09:13 +0200

    [Final Autorization]

--- core/frontend/views/sign.js
+++ core/frontend/views/sign.js
@@ -1,155 +1,170 @@
-var sign = function () {
-    var body = $('#page');
+$(function() {
 
-    var content = $('<div/>', {
-        id: 'login-form'
-    }).appendTo(body);
+    var sign = function () {
+        var body = $('#page');
 
-    var fieldset = $('<fieldset/>', {}).appendTo(content);
+        var content = $('<div/>', {
+            id: 'login-form'
+        }).appendTo(body);
 
-    var form = $('<form/>', {}).appendTo(fieldset);
+        var fieldset = $('<fieldset/>', {}).appendTo(content);
 
-    $('<h1/>', {
-        text: 'Bromo'
-    }).appendTo(form);
+        var form = $('<form/>', {}).appendTo(fieldset);
 
-    $('<p/>', {
-        text: 'Chat with your friends become more easier than ever.'
-    }).appendTo(form);
+        $('<h1/>', {
+            text: 'Bromo'
+        }).appendTo(form);
+
+        $('<p/>', {
+            text: 'Chat with your friends become more easier than ever.'
+        }).appendTo(form);
 
 
-    $('<input/>').attr({
-        type: 'email',
-        required: "true",
-        placeholder: "Email",
-        autocomplete: 'off'
-    }).appendTo(form);
+        $('<input/>').attr({
+            type: 'email',
+            required: "true",
+            placeholder: "Email",
+            autocomplete: 'off'
+        }).appendTo(form);
 
-    $('<input/>').attr({
-        type: 'password',
-        required: "true",
-        placeholder: "Password",
-        autocomplete: 'off'
-    }).appendTo(form);
+        $('<input/>').attr({
+            type: 'password',
+            required: "true",
+            placeholder: "Password",
+            autocomplete: 'off'
+        }).appendTo(form);
 
-    var sign = $('<input/>').attr({
-        value: 'Sign In',
-        type: 'submit'
-    }).appendTo(form);
+        var sign = $('<input/>').attr({
+            value: 'Sign In',
+            type: 'submit'
+        }).appendTo(form);
 
-    form.submit( function (e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: '/authorization',
-            data: {
-                email: $('[type = email]').val(),
-                password: $('[type = password]').val()
-            },
-            success: function(data) {
+        form.submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: '/authorization',
+                data: {
+                    email: $('[type = email]').val(),
+                    password: $('[type = password]').val()
+                },
+                success: function (data) {
 
-                if(data == true){
-                    body.empty();
-                    chat();
+                    if (data == true) {
+                        body.empty();
+                        chat();
+                    }
+
+                    else {
+
+                        sign.before($('<div/>', {
+                            class: 'error',
+                            text: 'Email or Password is incorrect!'
+                        }));
+
+                    }
                 }
-
-                else{
-
-                }
-            }
-        })
-    });
+            })
+        });
 
 
 //=====================================
 
-    var register = $('<div/>', {id: "register"}).appendTo(content);
+        var register = $('<div/>', {id: "register"}).appendTo(content);
 
-    $('<h5/>', {
-        text: "Don't have an Account?"
-    }).appendTo(register);
+        $('<h5/>', {
+            text: "Don't have an Account?"
+        }).appendTo(register);
 
 
+        $('<button/>', {
+            class: "btn",
+            click: function () {
 
-    $('<button/>', {
-        class: "btn",
-        click: function () {
+                form.remove();
+                register.remove();
+                //forgot.remove();
+                //sign.remove();
 
-            form.remove();
-            register.remove();
-            //forgot.remove();
-            //sign.remove();
+                var registration = $('<form/>', {}).appendTo(fieldset);
+                var error = $('<div/>', {id: "register"}).appendTo(content);
 
-            var registration = $('<form/>', {}).appendTo(fieldset);
-            var error = $('<div/>', {id: "register"}).appendTo(content);
+                $('<input/>').attr({
+                    type: 'email',
+                    required: "true",
+                    placeholder: "Email",
+                    autocomplete: 'off'
+                }).appendTo(registration);
 
-            $('<input/>').attr({
-                type: 'email',
-                required: "true",
-                placeholder: "Email",
-                autocomplete: 'off'
-            }).appendTo(registration);
+                $('<input/>').attr({
+                    type: 'password',
+                    required: "true",
+                    placeholder: "Password",
+                    autocomplete: 'off'
+                }).appendTo(registration);
 
-            $('<input/>').attr({
-                type: 'password',
-                required: "true",
-                placeholder: "Password",
-                autocomplete: 'off'
-            }).appendTo(registration);
-
-            $('<button/>', {
-                class: "reg", click: function () {
-                    $.ajax({
-                        type: "POST",
-                        url: '/registration',
-                        data: {
-                            email: $('[type = email]').val(),
-                            password: $('[type = password]').val(),
-                            username: $('[type = text]').val()
-                        },
-                        success: function(data) {
-
-                            console.log(data.result);
-
-                            if(data.result == true){
-                                body.empty();
-                                chat();
-                            }
-
-                            else{
+                $('<button/>', {
+                    class: "reg", click: function () {
+                        $.ajax({
+                            type: "POST",
+                            url: '/registration',
+                            data: {
+                                email: $('[type = email]').val(),
+                                password: $('[type = password]').val(),
+                                username: $('[type = text]').val()
+                            },
+                            success: function (data) {
 
                                 error.empty();
+                                console.log(data.result);
 
-                                $('<h5/>', {
-                                    text: data.error
-                                }).appendTo(error);
+                                if (data.result == true) {
+                                    body.empty();
+                                    chat();
+                                }
+
+                                if(data.error.indexOf("E11000") >= 0){
+                                    $('<h5/>', {
+                                        text: "That username is already exist!"
+                                    }).appendTo(error);
+                                }
+
+                                else {
+                                    $('<h5/>', {
+                                        text: data.error
+                                    }).appendTo(error);
+                                }
+
+                                console.log(data);
+                                main();
                             }
+                        });
+                    }
+                }).text("Create").appendTo(registration);
 
-                            console.log(data);
-                            main();
-                        }
-                    });
-                }}).text("Create").appendTo(registration);
+                $('[type = email]').before(
+                    $('<input/>', {
+                        type: 'text',
+                        required: "true",
+                        method: "get",
+                        placeholder: "Name",
+                        autocomplete: 'off'
+                    })
+                );
 
-            $('[type = email]').before(
-                $('<input/>', {
-                    type: 'text',
-                    required: "true",
-                    method: "get",
-                    placeholder: "Name",
-                    autocomplete: 'off'
-                })
-            );
+                $('[type = text]').addClass("reg");
+                $('[type = email]').addClass("reg");
+                $('[type = password]').addClass("reg");
 
-            $('[type = text]').addClass("reg");
-            $('[type = email]').addClass("reg");
-            $('[type = password]').addClass("reg");
+                registration.submit(function (e) {
+                    e.preventDefault();
+                });
 
-            registration.submit( function (e) {
-                e.preventDefault();
-            });
+            }
+        }).text('Create Account Now').appendTo(register);
 
-        }
-    }).text('Create Account Now').appendTo(register);
+    };
 
-};
+    sign();
+
+});
